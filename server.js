3015372537d348da6caa285b921d53c76ce4d94b
Ricardo Vazquez <rix501@gmail.com>
2015-08-29 19:36:42 -0400

    Remove unused route

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import r from 'rethinkdb';
 import { createDB, getGrupo, search, rsvp } from './db';
 
 const app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-app.get('/invitados', (req, res) => {
-  let conn;
-  r.connect(connectionInfo)
-  .then((c) => conn = c)
-  .then(() => r.table('invitados').run(conn))
-  .then((invitadosCursor) => invitadosCursor.toArray())
-  .then(res.json.bind(res));
-});
 
 app.get('/search', (req, res) => {
   search(req.query).then(res.json.bind(res));
